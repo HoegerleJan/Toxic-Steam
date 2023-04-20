fe83f89b248b1f4f6207e904695e6f5490d03475
JR1811 <janreichelt77@gmail.com>
2023-04-20 19:37:40 +0200

    fixed PlayerList restriction mixin (added PreLaunch entrypoint for MixinExtras)

--- src/main/java/net/doulrion/toxicsteam/mixin/InGameHudMixin.java
+++ src/main/java/net/doulrion/toxicsteam/mixin/InGameHudMixin.java
@@ -5,13 +5,14 @@ import net.doulrion.toxicsteam.init.ConfigInit;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.InGameHud;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
-
+@Debug(export = true)
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin extends DrawableHelper {
     @Final
@@ -23,12 +24,12 @@ public abstract class InGameHudMixin extends DrawableHelper {
         at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/KeyBinding;isPressed()Z", ordinal = 0)
     )
     private boolean toxicSteam$restrictPlayerListKeyPressed(boolean original) {
-        ToxicSteam.devLogger("started mixin evaluation");
+        ToxicSteam.devLogger("started mixin evaluation | original: " + original);
         if (!ConfigInit.CONFIG.restrictPlayerList) return original;
-        if (!client.options.playerListKey.isPressed()) return false;
-        if (client.player == null) return false;
-        ToxicSteam.devLogger("custom evaluation is used | client in singleplayer: " + client.isInSingleplayer());
-        return !client.isInSingleplayer();
-    }
+        if (client.player == null || client.isInSingleplayer()) return original;
+        if (client.player.isCreative() || client.player.isSpectator()) return original;
+        if (client.options.playerListKey.isPressed()) return false;
 
+        else return original;
+    }
 }
